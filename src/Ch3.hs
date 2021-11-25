3f516973179b6828827aa7d7c5f4f6ae7248df11
Oisín Mac Fhearaí <denpashogai@gmail.com>
2021-11-25 17:06:55 +0000

    ch3 exercises

--- src/Ch3.hs
+++ src/Ch3.hs
@@ -1,5 +1,7 @@
 module Ch3 where
 
+import Data.List (sortOn)
+
 data List a = Cons a (List a)
             | Nil
               deriving (Show)
@@ -8,7 +10,8 @@ fromList :: List a -> [a]
 fromList Nil = []
 fromList (Cons x xs) = x : fromList xs
 
-data UglyTree a = Node a (Maybe a) (Maybe a)
+data Tree a = Node a (Tree a) (Tree a) | Empty
+data UglyTree a = UglyNode a (Maybe a) (Maybe a)
 
 countElems :: [a] -> Int
 countElems [] = 0
@@ -19,3 +22,43 @@ calcMean xs = fromIntegral (sum xs) / fromIntegral (length xs)
 
 palindromify :: [a] -> [a]
 palindromify xs = xs ++ reverse xs
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
+
+sortByLength :: [[a]] -> [[a]]
+sortByLength = sortOn length
+
+intersperse :: a -> [[a]] -> [a]
+intersperse _ [] = []
+intersperse c (x:y:xs) = x ++ [c] ++ intersperse c (y:xs)
+intersperse c [x] = x
+
+height :: Tree a -> Int
+height Empty = 0
+height (Node _ l r) = 1 + max (height l) (height r)
+
+-- part 2 ex 9
+data Direction = DirLeft | DirRight | DirStraight deriving (Show, Eq)
+
+calcDirection :: (Ord a, Num a) => (a,a) -> (a,a) -> (a,a) -> Direction
+calcDirection (ax,ay) (bx,by) (cx,cy) | p == 0 = DirStraight
+                                      | p > 0 = DirLeft
+                                      | p < 0 = DirRight
+    where p = (bx - ax)*(cy - ay) - (cx - ax)*(by - ay)
+
+calcDirs :: (Ord a, Num a) => [(a, a)] -> [Direction]
+calcDirs (a:b:c:xs) = calcDirection a b c : calcDirs (b:c:xs)
+calcDirs _ = []
+
+-- tmp
+
+nth :: Int -> Maybe Integer
+nth 0 = Nothing
+nth n = pure $ primes !! (n - 1)
+
+primes :: [Integer]
+primes = [x | x <- 2:[3,5..], isPrime x]
+
+isPrime :: Integral a => a -> Bool
+isPrime x = not $ or [x `mod` n == 0 | n <- [2..(floor $ sqrt $ fromIntegral x)]]
--- test/Ch3Spec.hs
+++ test/Ch3Spec.hs
@@ -6,17 +6,47 @@ import Test.Hspec
 -- tests
 spec :: Spec
 spec = do
+
   describe "ex1" $
     it "converts a simple list" $
       fromList (Cons 'w' (Cons 'a' (Cons 't' Nil))) `shouldBe` "wat"
+
   describe "part 2 ex1" $
     it "counts properly" $
       countElems "wat" `shouldBe` 3
+
   describe "part 2 ex3" $
     it "calculates the mean" $
       calcMean [4,8,12] `shouldBe` 8
+
   describe "part 2 ex4" $
     it "palindromifies" $
       palindromify "cat" `shouldBe` "cattac"
 
+  describe "part 2 ex5" $ do
+    it "rejects non-palindrome" $
+      isPalindrome "cat" `shouldBe` False
+    it "accepts palindrome" $
+      isPalindrome "rotavator" `shouldBe` True
 
+  describe "part 2 ex6" $
+    it "sorts a list of lists" $
+      sortByLength ["cat", "a", "bratwurst", "suspects"] `shouldBe` ["a", "cat", "suspects", "bratwurst"]
+
+  describe "part 2 ex7" $
+    it "intersperses" $
+      intersperse ',' ["foo","bar","baz","quux"] `shouldBe` "foo,bar,baz,quux"
+
+  describe "part 2 ex8" $
+    it "counts height" $
+      height (Node "x" Empty (Node "y" Empty Empty)) `shouldBe` 2
+
+  describe "part 2 ex9" $ do
+    it "detects left" $
+      calcDirection (3,0) (6,5) (0,3) `shouldBe` DirLeft
+    it "detects right" $
+      calcDirection (3,0) (6,5) (8,2) `shouldBe` DirRight
+    it "detects forward straight" $
+      calcDirection (3,0) (6,5) (9,10) `shouldBe` DirStraight
+    it "detects backward straight" $
+      calcDirection (3,0) (6,5) (3,0) `shouldBe` DirStraight
